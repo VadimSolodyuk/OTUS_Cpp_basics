99b999672ae04d8c36c386fb29a79e4ccb236bb8
VadimSolodyuk <129878860+VadimSolodyuk@users.noreply.github.com>
2023-05-12 14:15:45 +0500

    Add files via upload

---
+++ 4.hw/src/astnode.cpp
@@ -0,0 +1,52 @@
+#include "astnode.hpp"
+
+ASTNode::ASTNode(const std::string &repr)
+    : repr_(repr)
+    , lhs_{nullptr}
+    , rhs_{nullptr} {}
+
+ASTNode::ASTNode(const std::string &repr, ASTNode *lhs, ASTNode *rhs)
+    : repr_(repr)
+    , lhs_{lhs}
+    , rhs_{rhs} {}
+
+ASTNode::~ASTNode() {
+    delete lhs_;
+    delete rhs_;
+}
+
+void ASTNode::print(std::ostream &out) const { inner_print(out, 0); }
+
+void ASTNode::inner_print(std::ostream &out, size_t indent) const {
+    
+    if (lhs_) {
+        lhs_->inner_print(out, indent + 1);
+    }
+    for (size_t i = 0; i < indent; ++i) {
+        out << "    ";
+    }
+    out << repr_ << '\n';
+    if (rhs_) {
+        rhs_->inner_print(out, indent + 1);
+    }
+}
+
+Number::Number(const int &val)
+        : ASTNode(std::to_string(val))
+        , val_(val) {}
+
+int Number::value() const { return val_; };
+
+Variable::Variable(const std::string &var)
+        : ASTNode(std::string(var))
+        , var_(var) {}
+
+std::string Variable::variable() const { return var_; };
+
+Operator::Operator(const std::string &oper, ASTNode *lhs, ASTNode *rhs)
+        : ASTNode(std::string(oper), lhs, rhs)
+        , oper_(oper)
+        , lhs_{lhs}
+        , rhs_{rhs} {}
+
+std::string Operator::oper() const { return oper_; };
---
+++ 4.hw/src/parser.cpp
@@ -0,0 +1,65 @@
+#include "parser.hpp"
+
+using Token = Lexer::Token;
+
+ASTNode *Parser::parse() { return expr(); }
+
+void Parser::next_token() { tok_ = lexer_.next_token(); }
+
+ASTNode *Parser::expr() {
+    // parse addition and subsctruction
+    ASTNode *root = term();
+    for (;;) {
+        switch (tok_) {
+        case Token::Operator: {
+            std::string op = lexer_.get_operator();
+            if (op.front()== '+' || op.front()== '-') {
+                root = new Operator(op, root, term());
+                break;
+            }
+            return root;
+            break;
+        }
+        default:
+            return root;
+        }
+    }
+}
+
+ASTNode *Parser::term() {
+    // parse multiplication and division
+    ASTNode *root = prim();
+    for (;;) {
+        switch (tok_) {
+        case Token::Operator: {
+            std::string op = lexer_.get_operator();
+            if (op.front()== '*' || op.front()== '/') {
+                root = new Operator(op, root, prim());
+                break;
+            }
+            return root;
+            break;
+        }
+        default:
+            return root;
+        }
+    }
+}
+
+ASTNode *Parser::prim() {
+    // parse numbers and names
+    ASTNode *node = nullptr;
+    next_token();
+    switch (tok_) {
+    case Token::Number:
+        node = new Number(lexer_.get_number());
+        break;
+    case Token::Name:
+        node = new Variable(lexer_.get_name());
+        break;
+    default:
+        break;
+    }
+    next_token();
+    return node;
+}
