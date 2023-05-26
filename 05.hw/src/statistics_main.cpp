0201e100f8e181f5457744ada83881be3add7c51
VadimSolodyuk <129878860+VadimSolodyuk@users.noreply.github.com>
2023-05-26 18:17:03 +0500

    Add files via upload

---
+++ 05.hw/src/statistics_main.cpp
@@ -0,0 +1,45 @@
+#include "../include/Max.h"
+#include "../include/Min.h"
+#include "../include/Mean.h"
+#include "../include/Std.h"
+
+#include <iostream>
+
+
+int main() {
+
+	const size_t statistics_count = 4;
+	IStatistics *statistics[statistics_count];
+
+	statistics[0] = new Min{};
+	statistics[1] = new Max{};
+	statistics[2] = new Mean{};
+	statistics[3] = new Std{};
+
+
+	double val = 0;
+	while (std::cin >> val) {
+		for (size_t i = 0; i < statistics_count; ++i) {
+			statistics[i]->update(val);
+		}
+	}
+
+	// Handle invalid input data
+	if (!std::cin.eof() && !std::cin.good()) {
+		std::cerr << "Invalid input data\n";
+		return 1;
+	}
+
+	// Print results if any
+	std::cout << "\n";
+	for (size_t i = 0; i < statistics_count; ++i) {
+		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+	}
+
+	// Clear memory - delete all objects created by new
+	for (size_t i = 0; i < statistics_count; ++i) {
+		delete statistics[i];
+	}
+
+	return 0;
+};
